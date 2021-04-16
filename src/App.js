56c72c39b2e17979ca54d1ae79ccfd65f3e8a481
Shanel Beebe <shanelbeebe@icloud.com>
2021-04-15 18:35:47 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+const getSnacks = () => {
+const myHeaders = new Headers();
+myHeaders.append("x-app-id", "df445c8d");
+myHeaders.append("x-app-key", "aafcc21abbff4ba8ef87b9892cb2d5a9");
+
+
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://trackapi.nutritionix.com/v2/search/instant?query=butter cookies&detailed=true", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+      const snacks = result.branded
+      console.log(snacks)
+      snacks.map(snack => {
+          const nutrientList = snack.full_nutrients
+          const snackName = snack.food_name
+          const snackImg = snack.photo.thumb
+          const sugarContent = nutrientList.filter(nutrient => nutrient.attr_id === 269)
+          console.log(snackName, snackImg, sugarContent[0].value)
+          return snackName;
+      })
+  })
+  .catch(error => console.log('error', error));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>A Bitter Parent</h1>
+      <button onClick={getSnacks}>Click Me</button>
     </div>
   );
 }
